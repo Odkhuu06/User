c25b57b69e7a3367502c4eea9a41d1a759c140cd
25LP2339 25LP2339 <25LP2339@G303-CC0136.local>
2025-06-17 17:56:12 +0800

    ak

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
 import express, { Request, Response } from "express"
-import fs from "fs-extra"
+import userRouter from "./router/user/user"
 const app = express()
 const port = 3000
 app.use(express.json())
-app.get("/", (req: Request, res: Response)=>{
-    res.send({name: "odhuu", age: 30})
-})
+app.get("/", (req: Request, res: Response) => {
+//   res.send("users");
+res.send("hii")
+});
+app.use("/", userRouter)
 
-app.post("/user", (req: Request, res: Response)=>{
-    const {name, age }: {name: string, age: number}= req.body
-    res.json({mesagge:`User ${name} is ${age} years old`})
-})
-
-
-app.put("/updateUser", (req: Request, res: Response)=>{
-    const {name, age }: {name: string, age: number}= req.body
-    res.json(`Updated ${name} ${age}`)
-})
 app.listen(port, ()=>{
     console.log(`http://localhost:${port}`);
     
 })
+// authentication
+
+// user create -> name, age, userName(unique), userEmail, phoneNumber, password
+// user login -> userName, password
+// user delete -> userId eer ustgana
+// user update -> userId eer ni update
+// get user by id -> userId aar zowhon neg useriin medeelel awna.
+
+// fs-extra gdeg dependency ashiglana.
---
+++ src/router/user/user.ts
@@ -0,0 +1,100 @@
+import  express, { Request, Response }  from "express"
+import { nanoid } from "nanoid";
+import { User } from "../../types/types";
+import fs from "fs-extra";
+
+const userRouter = express.Router();
+
+userRouter.get("/users", (req: Request, res: Response) => {
+  const users = fs.readFileSync("./user.json", { encoding: "utf8", flag: "r" });
+  res.json(JSON.parse(users));
+});
+
+userRouter.post("/createUser",(req: Request, res: Response) => {
+      console.log("req.body:", req.body); 
+  const { name, age, userName, userEmail, phoneNumber, password }: User =
+    req.body;
+
+//   if (!password) {
+//     return res.status(400).json({ error: "Password is required" });
+//   }
+//  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const uniqueId = nanoid();
+
+  const filePath = "./user.json";
+
+  let users: User[] = [];
+
+  if (fs.existsSync(filePath)) {
+    const existingData = fs.readFileSync(filePath, "utf8");
+    if (existingData.trim().length > 0) {
+      users = JSON.parse(existingData);
+    }
+  }
+
+  users.push({
+    name,
+    age,
+    userId: uniqueId,
+    userName,
+    userEmail,
+    phoneNumber,
+    password,
+  });
+
+  fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+
+  res.send("Successfully created User");
+});
+
+userRouter.post("/deleteUser", (req: Request, res: Response) => {
+  const { userId } = req.body;
+  const existingData = fs.readFileSync("./user.json", "utf8");
+
+  const deletedUser = JSON.parse(existingData).filter(
+    (user: any) => user.userId !== userId
+  );
+
+  fs.writeFileSync("./user.json", JSON.stringify(deletedUser, null, 2));
+
+  res.json({
+    userId,
+  });
+});
+
+userRouter.put("/updateUser", (req: Request, res: Response) => {
+  const { name, age, userId }: { name: string; age: number; userId: string } =
+    req.body;
+  const existingData = fs.readFileSync("./user.json", "utf8");
+
+  const updatedUser = JSON.parse(existingData).map((user: any) => {
+    if (user.userId === userId) {
+      return { ...user, name: name, age: age };
+    }
+  });
+
+  fs.writeFileSync("./user.json", JSON.stringify(updatedUser, null, 2));
+
+  res.json(updatedUser);
+});
+
+export default userRouter;
+// const userRouter = express.Router()
+
+
+// userRouter.get("/", (req: Request, res: Response)=>{
+//     res.send({name: "odhuu", age: 30})
+// })
+
+// userRouter.post("/user", (req: Request, res: Response)=>{
+//     const {name, age }: {name: string, age: number}= req.body
+//     res.json({mesagge:`User ${name} is ${age} years old`})
+// })
+
+
+// userRouter.put("/updateUser", (req: Request, res: Response)=>{
+//     const {name, age }: {name: string, age: number}= req.body
+//     res.json(`Updated ${name} ${age}`)
+// })
+// export default userRouter;
